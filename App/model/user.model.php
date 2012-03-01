90d2c03aa537a8eee892262de10e641884d93846
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-03-01 08:46:26 -0300

    - updating models

--- App/model/user.model.php
+++ App/model/user.model.php
@@ -12,22 +12,7 @@
 		private $attr = array('id'=>NULL, 'username'=>NULL, 'email'=>NULL, 'password'=>NULL, 'typeuser'=>NULL, 'active'=>NULL);
 
 		function __construct($param = NULL) {			
-
-			if (is_array($param)) {
-				if (Utility::isAssoc($param)){ 
-					foreach ($param as $item) {
-						$key = key($param);
-						if (isset($param[$key])) {
-							if (key_exists($key, $this->attr)) {
-								$this->attr[$key] = $item;
-							}
-						}
-						next($param);
-					}				
-				} else {
-					
-				}
-			}
+			$this->put($param);
 		}
 
 	//    public function __destruct() {
@@ -39,7 +24,7 @@
 
 		public function __set($name, $value) {
 			//echo "Setting '$name' to '$value'\n";
-			$this->attr[$name] = $value;
+			$this->attr[$name] = $this->validate($name, $value);
 		}
 
 		public function __get($name) {
@@ -64,6 +49,41 @@
 		public function __unset($name) {
 			//echo "Unsetting '$name'\n";
 			unset($this->$attr[$name]);
+		}
+		private function validate($name, $value) {
+			//add validation based on $name
+			return $value;
+		}
+		public function put($param = NULL) {
+			//set a group of property based on an array input
+			$semID = FALSE;
+			//QUICK SKIP if ID is ommited
+			if (count($param) == count($this->attr)-1) {
+				$semID = TRUE;
+			}
+			
+			if (is_array($param)) {
+				//if the array passes by objest by association or index
+				if (Utility::isAssoc($param)){ 
+					foreach ($param as $key => $item) {
+						//Preserve data integrity and filter out non-attributes
+						if (key_exists($key, $this->attr)) {
+							$this->attr[$key] = $this->validate($key, $item);
+						}
+					}				
+				} else {
+					
+					foreach ($this->attr as $key => $val) {
+						if (($key == 'id')&&($semID)) {continue;}//skipping id update
+						$this->attr[$key] = $this->validate($key, array_shift($param));
+					}
+					
+				}
+			}
+		}
+		
+		public function __toString() {
+			return json_encode(get_object_vars($this));
 		}
 
 	}
