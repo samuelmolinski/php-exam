3c003da400ad74c3c372ba70b438ca234cc2b97f
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-03-01 08:48:32 -0300

    -update core

--- App/inspect.php
+++ App/inspect.php
@@ -1,65 +1,58 @@
 <?php
+	
+	require_once 'config.php';
+
     function inspect($var){
-	
-	$bt = debug_backtrace();
-	$src = file($bt[0]["file"]);
-	$line = $src[ $bt[0]['line'] - 1 ];
-	
-	//striping the inspect() from the sting
-	$strip = explode('inspect(', $line);
-	$matches = preg_match('#\(#', $strip[0]);
-	$strip = explode(')', $strip[1]);
-	for ($i=0;$i<count($matches-1);$i++) {
-		array_pop($strip);
-	}
-	$label = implode(')', $strip);
-	//maybe a better way
-	/*
-	$max = strlen($match[1]);
-	$varname = "";
-	$c = 0;
-	for($i = 0; $i < $max; $i++){
-		if(     $match[1]{$i} == "(" ) $c++;
-		elseif( $match[1]{$i} == ")" ) $c--;
-		if($c < 0) break;
-		$varname .=  $match[1]{$i};
-	}
-	$label = $varname;
-	 * */
-	
-	
-	$colorVar = 'Blue';
-		
-	if (is_bool($var)) {
-		$colorType = 'Green';
-		$type = 'bool';
-	} elseif (is_string($var)) {
-		$colorType = 'DarkOrange';
-		$type = 'string';
-	} elseif (is_array($var)) {
-		$colorType = 'DarkOrchid';	
-		$type = 'array';		
-	} elseif (is_object($var)) {
-		$colorType = 'BlueViolet';	
-		$type = 'object';		
-	} elseif (is_numeric($var)) {
-		$colorType = 'Red';	
-		$type = 'numeric';
-	} else {
-		$type = 'unknown';		
-		$colorType = 'Tomato';	
-	}
-	
-	echo "<div style='background-color:#FFF; overflow:visible;'><pre><span style='color:$colorVar'>";
-		echo $label;
-		echo "</span> = <span style='color:$colorType'>";
-		if ($type == 'string') {
-			print_r(htmlspecialchars($var));
-		} else {
-			print_r($var);
+				
+		//		
+		if (!defined('DEBUG') || DEBUG) {
+			
+			$bt = debug_backtrace();
+			$src = file($bt[0]["file"]);
+			$line = $src[ $bt[0]['line'] - 1 ];
+
+			//striping the inspect() from the sting
+			$strip = explode('inspect(', $line);
+			$matches = preg_match('#\(#', $strip[0]);
+			$strip = explode(')', $strip[1]);
+			for ($i=0;$i<count($matches-1);$i++) {
+				array_pop($strip);
+			}
+			$label = implode(')', $strip);
+
+			$colorVar = 'Blue';
+
+			if (is_bool($var)) {
+				$colorType = 'Green';
+				$type = 'bool';
+			} elseif (is_string($var)) {
+				$colorType = 'DarkOrange';
+				$type = 'string';
+			} elseif (is_array($var)) {
+				$colorType = 'DarkOrchid';	
+				$type = 'array';		
+			} elseif (is_object($var)) {
+				$colorType = 'BlueViolet';	
+				$type = 'object';		
+			} elseif (is_numeric($var)) {
+				$colorType = 'Red';	
+				$type = 'numeric';
+			} else {
+				$type = 'unknown';		
+				$colorType = 'Tomato';	
+			}
+
+			echo "<div style='background-color:#FFF; overflow:visible;'><pre><span style='color:$colorVar'>";
+			echo $label;
+			echo "</span> = <span style='color:$colorType'>";
+			if ($type == 'string') {
+				print_r(htmlspecialchars($var));
+			} else {
+				print_r($var);
+			}
+			echo "</span></pre></div>";
 		}
-		echo "</span></pre></div>";
-		
-}
+
+	}
 
 ?>
--- App/test.php
+++ App/test.php
@@ -1,15 +1,14 @@
 <?php
 
-	error_reporting(E_ALL);
 	if (!defined('ABSPATH')) {
 		define('ABSPATH', dirname(__FILE__) . '/');
 	}
 
 	//required files
-	require_once (ABSPATH . 'testing/AllTests.php');
-	require_once (ABSPATH . 'DBinstaller.php');
 	require_once (ABSPATH . 'config.php');
 	require_once (ABSPATH . 'inspect.php');
+	require_once (ABSPATH . 'DBinstaller.php');
+	require_once (ABSPATH . 'testing/AllTests.php');
 
 	//$testDB = new DBinstaller(TEST_DB_HOST, TEST_DB_USER, TEST_DB_PASSWORD, TEST_DB_NAME);
 	$testDB = new DBinstaller();
