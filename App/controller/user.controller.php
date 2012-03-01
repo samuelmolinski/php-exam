4ad65c4ce387fc400d8f7e6ade6a95381b555c76
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-03-01 08:47:40 -0300

    - updating controllers

--- App/controller/user.controller.php
+++ App/controller/user.controller.php
@@ -9,7 +9,8 @@ class User_Controller extends DAO {
 	public $user;
 	private $controller;
 
-	function __construct($param = NULL) {
+	function __construct($param = NULL) {		
+		parent::__construct("mysql:dbname=" . DB_NAME . ";host=" . DB_HOST, DB_USER, DB_PASSWORD);
 		if (is_array($param)) {
 			$this->user = new User_Model($param);
 		} else {
@@ -22,42 +23,41 @@ class User_Controller extends DAO {
 
 		$this->table = TB_USER;
 		$this->fields = "chr_email, chr_password, chr_username, int_id_typeuser, bln_active";
-		$this->values = "
-                        '" . $this->user->email . "', 
-                        '" . $this->user->password . "',
-                        '" . $this->user->username . "',
-                        '" . $this->user->typeuser . "',
-                        '" . $this->user->active . "'";
+		$this->values = "'" . $this->user->email . 
+						"','" . $this->user->password . 
+						"','" . $this->user->username . 
+						"','" . $this->user->typeuser . 
+						"','" . $this->user->active . "'";
 
-		$this->insert();
+		$t = $this->insert();
+		//update with the retrived id
+		$dao = $this->getDAO();
+		$this->user->id = $this->id = $dao->lastInsertId();
 	}
 
 	public function actionUpdate() {
-
 		$this->table = TB_USER;
-		$this->fields = "
-                        chr_username  = '" . $this->user->username . "', 
-                        chr_email = '" . $this->user->email . "',
-                        chr_password = '" . $this->user->password . "', 
-                        int_id_typeuser = '" . $this->user->typeuser . "'
-                        bln_active = '" . $this->user->active . "'
-                ";
-
+		$this->fields =   " chr_username  = '" . $this->user->username . 
+						"', chr_email = '" . $this->user->email . 
+						"', chr_password = '" . $this->user->password . 
+						"', int_id_typeuser = '" . $this->user->typeuser . 
+						"', bln_active = '" . $this->user->active . "'";
+		$this->where = "int_id_user = '". $this->user->id . "'";
 		$this->update();
 	}
 
 	public function actionList() {
-		$this->table = TB_CLIENTE;
-		$this->fields = "chr_email, chr_username, id_typeuser, bln_active";
+		$this->table = TB_USER;
+		$this->fields = "chr_username, chr_email, chr_password, int_id_typeuser, bln_active";
 		$this->asc = "chr_username";
 		$this->limit = 20;
 
-		$this->select();
-		$this->total();
+		//returning values as a single object or an array of those objects
+		return Utility::formatArraySingle($this->select(), 'User_Model');		
 	}
 
 	public function actionSearch($search) {
-		$query_default = Array('table' => TB_USER, 'fields' => "*", 'asc' => "chr_email", 'where' => "int_id_user", 'like' => "*", 'limit' => "20");
+		$query_default = array('table' => TB_USER, 'fields' => "*", 'asc' => "chr_email", 'where' => "int_id_user", 'like' => "*", 'limit' => "20");
 		if (is_array($search)) {
 			$query = array_merge($query_default, $search);
 		} else {
@@ -70,45 +70,17 @@ class User_Controller extends DAO {
 		$this->where = $query['where'];
 		$this->like = $query['like'];
 		$this->limit = $query['limit'];
-
-		$return = array();
-		$list = $this->select(); 
-		foreach ($list as $item) {
-			$return[] = new User_Model($item);
-		}
-		$this->total();
-		return $return;
-	}
-	
-	public function actionGet($search) {
-		$query_default = Array('table' => TB_USER, 'fields' => "*", 'where' => "int_id_user");
-		if (is_array($search)) {
-			$query = array_merge($query_default, $search);
-		} else {
-			$query = $query_default;
-			$query['where'] = "int_id_user = " . $search;
-		}
-		$this->table = $query['table'];
-		$this->fields = $query['fields'];
-		$this->where = $query['where'];
-
-		$list = $this->select(); 
-		foreach ($list as $item) {
-			$return[] = new User_Model($item);
-		}
-		$this->total();
-		if (count($return)>1) {
-			return $return;
-		} else {			
-			return $return[0];
-		}
+		
+		//returning values as a single object or an array of those objects
+		return Utility::formatArraySingle($this->select(), 'User_Model');
+		
 	}
 
-	public function actionDelete() {
-		$this->setId($_POST['id']);
+	public function actionDelete($id) {
 		$this->table = TB_USER;
+		$this->where = "int_id_user = $id";
 
-		$this->update();
+		$this->delete();
 	}
 
 }
